c83ec9945acff752a1c586eefab876363cd3ac2b
Alvaro <alvaro.sebastian98@gmail.com>
2019-10-28 15:35:32 -0500

    Inserción inicial en el repositorio del módulo Calculator

---
+++ Calculator/ViewModels/OperacionesViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Ejercicio08.ViewModels
+{
+    public class OperacionesViewModel : ViewModelBase
+    {
+
+        #region Propiedades
+
+        int currentState = 1;
+        string mathOperator;
+        double firstNumber, secondNumber;
+
+        string valor;
+        public string Valor
+        {
+            get { return valor; }
+            set
+            {
+                if (valor != value)
+                {
+                    valor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        public void OnSelectNumber(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+
+            if (Valor == "0"|| currentState < 0)
+            {
+                Valor = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            Valor += pressed;
+
+            double number;
+            if (double.TryParse(Valor, out number))
+            {
+                Valor = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+            }
+        }
+
+        public void OnSelectOperator(object sender, EventArgs e)
+        {
+            currentState = -2;
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            mathOperator = pressed;
+        }
+
+        public void OnClear(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            Valor = "0";
+        }
+
+        public void OnCalculate(object sender, EventArgs e)
+        {
+            if (currentState == 2)
+            {
+                var result = SimpleCalculator.Calculate(firstNumber, secondNumber, mathOperator);
+
+                Valor = result.ToString();
+                firstNumber = result;
+                currentState = -1;
+            }
+        }       
+
+    }
+}
---
+++ Calculator/Views/Calculadora.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator.Views
+{
+    public partial class Calculadora : ContentPage
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+            this.BindingContext = new OperacionesViewModel();
+        }
+    }
+}
